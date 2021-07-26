82e29de4c52053ef8f5b76cfb624dd77a5c571c5
xxenvious <87677233+xxenvious@users.noreply.github.com>
2021-07-26 01:41:54 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minutesEl = document.getElementById("minutes");
+const secondsEl = document.getElementById("seconds");
+
+const newYears = "1 Sep 2021 10:00:00";
+
+function countdown() {
+    const dragonConDate = new Date(newYears);
+    const currentDate = new Date();
+
+    const totalSeconds = (dragonConDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minutesEl.innerHTML = formatTime(minutes);
+    secondsEl.innerHTML = formatTime(seconds);
+}
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+// initial call
+countdown();
+
+setInterval(countdown, 1000);
